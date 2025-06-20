a0eff6f3f9dc70ad96da29c45751c3c2430f44fe
khai <khaitruong1301@gmail.com>
2025-06-21 00:13:53 +0700

    buoi13_authentication

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using System.Security.Claims;
+using System.Text;
+using Blazored.LocalStorage;
 using dotnet03_web_blazor.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Components.Authorization;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +13,48 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+//JWT
+// 1. Đọc key, issuer và audience từ appsettings.json
+var key = builder.Configuration["jwt:key"];           // Khóa bí mật để ký token
+var issuer = builder.Configuration["jwt:issuer"];     // Issuer (bên phát hành token)
+var audience = builder.Configuration["jwt:audience"]; // Audience (người nhận token)
+Console.WriteLine($@"{key} - {issuer} - {audience}");
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true, // Xác thực key bí mật của token
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+                    ValidateIssuer = true,// Xác thực Issuer 
+                    ValidIssuer = issuer, // Phải khớp với Issuer trong token
+                    ValidateAudience = true,    // Xác thực Audience
+                    ValidAudience = audience, // Phải khớp với Audience trong token
+                    ValidateLifetime = true, // Xác thực thời gian hết hạn của token
+                    ClockSkew = TimeSpan.Zero, // Bỏ qua độ trễ thời gian giữa server và client (ngăn lỗi thời gian)
+                    RoleClaimType = ClaimTypes.Role, // Ánh xạ claim role, (blazor auth client)
+                    NameClaimType = ClaimTypes.Name,
+                };
+            });
+
+//Xét cứng 2 quyền kiểm tra
+builder.Services.AddAuthorization(options =>
+{
+          // Chính sách chỉ cho phép Admin truy cập
+          options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+          // Chính sách chỉ cho phép User truy cập
+          options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+});
+// 4. Thêm AuthorizationCore để sử dụng trong Blazor Components (Phần view)
+builder.Services.AddAuthorizationCore();
+
+//5. DI JWT service vừa tạo
+builder.Services.AddScoped<JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+
+// DI localstorage
+builder.Services.AddBlazoredLocalStorage();
 
 //DI service http
 builder.Services.AddHttpClient();
@@ -20,13 +67,15 @@ builder.Services.AddScoped<ProductResfulService>();
 builder.Services.AddScoped<RoomService>();
 
 
-builder.Services.AddCors(option=>{
-    option.AddPolicy("allow_origin", policy => {
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("allow_origin", policy =>
+    {
         // policy.AllowAnyOrigin(); //Cho phép tất cả các client đều có thể gửi dữ liệu đến server
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("http://127.0.0.1:5501", "*")
             .AllowAnyHeader() //Cho phép rq tất cả header
-            .AllowAnyMethod(); //Cho phép rq tất cả method (POST,PUT,GET,DELETE,OPTION)
-            // .AllowCredentials(); ////Cho phép cookie...
+            .AllowAnyMethod() //Cho phép rq tất cả method (POST,PUT,GET,DELETE,OPTION)
+            .AllowCredentials(); ////Cho phép cookie...
     });
 });
 
@@ -53,6 +102,9 @@ if (!app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI();
 
+//Middle ware phân quyền
+app.UseAuthentication();  //401 : lỗi xác thực
+app.UseAuthorization(); //403: forbiden lỗi chưa đủ quyền
 
 app.UseHttpsRedirection();
 
--- obj/Debug/net9.0/dotnet03_web_blazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet03_web_blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet03_web_blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56224b83037423acbee575f6489bc9bbad703081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815a5f491efea84fdbfd8ed3cfbf7264d011d73c")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet03_web_blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet03_web_blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
